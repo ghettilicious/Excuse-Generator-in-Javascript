ff5917bc1ef42fec1bfc5a2a391f2204569255da
ghettilicious <158236766+ghettilicious@users.noreply.github.com>
2024-02-16 19:28:23 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,51 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = [
+    "My neighbor ",
+    "The dog ",
+    "Spiderman ",
+    "The Principal ",
+    "My dad "
+  ];
+  let action = ["ate ", "peed on ", "crushed ", "broke ", "tore up "];
+
+  let what = [
+    "my homework ",
+    "my laptop ",
+    "the car ",
+    "my shoes ",
+    "my clothes "
+  ];
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actIndx = Math.floor(Math.random() * action.length);
+  let whaIndx = Math.floor(Math.random() * what.length);
+  let wheIndx = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndx] +
+    "" +
+    action[actIndx] +
+    "" +
+    what[whaIndx] +
+    "" +
+    when[wheIndx]
+  );
 };
